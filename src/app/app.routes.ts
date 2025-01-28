61166cd3e81ba66c0ed7647f8fd98644f330fed6
kouki abderrahmen <abderrahmen.kouki@polytechnicien.tn>
2025-01-28 10:29:42 +0100

    errort

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Comp1Component } from './comp1/comp1.component';
 import { Routes } from '@angular/router';
 import { Comp2Component } from './comp2/comp2.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
   { path: 'chemain1', component: Comp1Component },
-  { path: 'chemain2', component: Comp2Component }
+  { path: 'chemain2', component: Comp2Component },
+  {path : '**', component : ErrorComponent}
 ];
